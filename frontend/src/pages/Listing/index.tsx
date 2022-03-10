c0af01b740c53b0b5ba1df3bb495e57002489117
MthAlvarez <99270229+MthAlvarez@users.noreply.github.com>
2022-03-10 08:28:27 -0300

    Add files via upload

---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,36 @@
+import MovieCard from "components/MovieCard";
+import Pagination from "components/Pagination";
+
+function Listing(){
+    return(
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Listing;
